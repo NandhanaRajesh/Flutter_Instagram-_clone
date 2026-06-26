a9e07e23143466281a9f30bca3f5fa5fcd18a5cf
NandhanaRajesh <nandhanarajesh27@gmail.com>
2026-06-26 15:47:20 +0530

    likes added on each post

--- lib/features/home/widgets/post_card.dart
+++ lib/features/home/widgets/post_card.dart
@@ -2,13 +2,25 @@ import 'package:flutter/material.dart';
 
 import '../models/post.dart';
 
-class PostCard extends StatelessWidget {
+class PostCard extends StatefulWidget {
   final Post post;
 
   const PostCard({
     super.key,
     required this.post,
   });
+
+  @override
+  State<PostCard> createState() => _PostCardState();
+}
+class _PostCardState extends State<PostCard> {
+  late int likeCount;
+  bool isLiked = false;
+@override
+void initState() {
+  super.initState();
+  likeCount = widget.post.likes;
+}
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +41,7 @@ class PostCard extends StatelessWidget {
                 ),
                 const SizedBox(width: 10),
                 Text(
-                  post.username,
+                  widget.post.username,
                   style: const TextStyle(
                     fontWeight: FontWeight.bold,
                   ),
@@ -42,7 +54,7 @@ class PostCard extends StatelessWidget {
            ClipRRect(
              borderRadius: BorderRadius.circular(8),
                child: Image.network(
-               post.imageUrl,
+               widget.post.imageUrl,
                height: 300,
                 width: double.infinity,
                 fit: BoxFit.cover,
@@ -51,28 +63,46 @@ class PostCard extends StatelessWidget {
 
             const SizedBox(height: 12),
 
-            const Row(
-              children: [
-                Icon(Icons.favorite_border),
-                SizedBox(width: 16),
-                Icon(Icons.chat_bubble_outline),
-                SizedBox(width: 16),
-                Icon(Icons.send),
-              ],
-            ),
+            Row(
+               children: [
+                 IconButton(
+                  onPressed: () {
+        setState(() {
+          isLiked = !isLiked;
 
+          if (isLiked) {
+            likeCount++;
+          } else {
+            likeCount--;
+          }
+        });
+      },
+      icon: Icon(
+        isLiked ? Icons.favorite : Icons.favorite_border,
+        color: isLiked ? Colors.red : null,
+      ),
+    ),
+
+    const SizedBox(width: 8),
+
+    const Icon(Icons.chat_bubble_outline),
+
+    const SizedBox(width: 16),
+
+    const Icon(Icons.send),
+  ],
+),
             const SizedBox(height: 10),
 
             Text(
-              '${post.likes} likes',
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
+  '$likeCount likes',
+  style: const TextStyle(
+    fontWeight: FontWeight.bold,
+  ),
+),
             const SizedBox(height: 5),
 
-            Text(post.caption),
+            Text(widget.post.caption),
           ],
         ),
       ),
