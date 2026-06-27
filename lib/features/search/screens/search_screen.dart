031d2d8c71dbd196543d3f4394a711187930afd5
NandhanaRajesh <nandhanarajesh27@gmail.com>
2026-06-27 19:56:53 +0530

    search page added with images below

--- lib/features/search/screens/search_screen.dart
+++ lib/features/search/screens/search_screen.dart
@@ -5,10 +5,41 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Search Screen',
-        style: TextStyle(fontSize: 24),
+    final images = List.generate(
+      30,
+      (index) => 'https://picsum.photos/300/300?random=$index',
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          decoration: InputDecoration(
+            hintText: 'Search',
+            prefixIcon: const Icon(Icons.search),
+            filled: true,
+            fillColor: Colors.grey.shade200,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide.none,
+            ),
+            contentPadding: EdgeInsets.zero,
+          ),
+        ),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(2),
+        itemCount: images.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 2,
+          mainAxisSpacing: 2,
+        ),
+        itemBuilder: (context, index) {
+          return Image.network(
+            images[index],
+            fit: BoxFit.cover,
+          );
+        },
       ),
     );
   }
